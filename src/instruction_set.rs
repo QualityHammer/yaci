17b5bc1d1c17abdd9668d9cff85ced61ee9cd6ad
QualityHammer <agingllama@gmail.com>
2020-10-13 02:10:17 -0400

    Changed usize back to u16.

--- src/instruction_set.rs
+++ src/instruction_set.rs
@@ -1,7 +1,7 @@
 mod opcodes {
     use crate::Chip8InterpreterRef;
 
-    type Instruction = fn(Chip8InterpreterRef, usize) -> Option<()>;
+    type Instruction = fn(Chip8InterpreterRef, u16) -> Option<()>;
 
     const FIRST_DIGIT_OPCODE_COUNT: usize = 12;
     const DIGIT_0_OPCODE_COUNT: usize = 3;
@@ -11,38 +11,38 @@ mod opcodes {
 
     use first_digit as d;
 
-    const FIRST_DIGIT_OPCODES: [usize; 7] = [0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7];
+    const FIRST_DIGIT_OPCODES: [u16; 7] = [0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7];
     const FIRST_DIGIT_INSTRUCTIONS: [Instruction; 7] =
         [d::_1, d::_2, d::_3, d::_4, d::_5, d::_6, d::_7];
 
     use digit_0 as d0;
 
-    const DIGIT_0_OPCODES: [usize; DIGIT_0_OPCODE_COUNT] = [0x0, 0xE0, 0xEE];
+    const DIGIT_0_OPCODES: [u16; DIGIT_0_OPCODE_COUNT] = [0x0, 0xE0, 0xEE];
     const DIGIT_0_INSTRUCTIONS: [Instruction; DIGIT_0_OPCODE_COUNT] = [d0::_0, d0::_e0, d0::_ee];
 
     use digit_8 as d8;
 
-    //const DIGIT_8_OPCODES: [usize; DIGIT_8_OPCODE_COUNT] = [];
+    //const DIGIT_8_OPCODES: [u16; DIGIT_8_OPCODE_COUNT] = [];
     //const DIGIT_8_INSTRUCTIONS: [Instruction; DIGIT_8_OPCODE_COUNT] = [];
 
     use digit_e as de;
 
-    //const DIGIT_E_OPCODES: [usize; DIGIT_E_OPCODE_COUNT] = [];
+    //const DIGIT_E_OPCODES: [u16; DIGIT_E_OPCODE_COUNT] = [];
     //const DIGIT_E_INSTRUCTIONS: [Instruction; DIGIT_E_OPCODE_COUNT] = [];
 
     use digit_f as df;
 
-    //const DIGIT_F_OPCODES: [usize; DIGIT_F_OPCODE_COUNT] = [];
+    //const DIGIT_F_OPCODES: [u16; DIGIT_F_OPCODE_COUNT] = [];
     //const DIGIT_F_INSTRUCTIONS: [Instruction; DIGIT_F_OPCODE_COUNT] = [];
 
     use std::collections::HashMap;
 
     pub struct Chip8InstructionSet {
-        first_digit: HashMap<usize, Instruction>,
-        digit_0: HashMap<usize, Instruction>,
-        digit_8: HashMap<usize, Instruction>,
-        digit_e: HashMap<usize, Instruction>,
-        digit_f: HashMap<usize, Instruction>,
+        first_digit: HashMap<u16, Instruction>,
+        digit_0: HashMap<u16, Instruction>,
+        digit_8: HashMap<u16, Instruction>,
+        digit_e: HashMap<u16, Instruction>,
+        digit_f: HashMap<u16, Instruction>,
     }
 
     impl Chip8InstructionSet {
@@ -51,7 +51,7 @@ mod opcodes {
             let mut digit_0 = HashMap::with_capacity(DIGIT_0_OPCODE_COUNT);
             for (op, func) in DIGIT_0_OPCODES
                 .iter()
-                .map(|o| *o as usize)
+                .map(|o| *o as u16)
                 .zip(DIGIT_0_INSTRUCTIONS.iter().map(|f| *f as Instruction))
             {
                 digit_0.insert(op, func);
@@ -75,14 +75,14 @@ mod opcodes {
         use super::*;
 
         // Jump to address
-        pub fn _1(chip: Chip8InterpreterRef, address: usize) -> Option<()> {
+        pub fn _1(chip: Chip8InterpreterRef, address: u16) -> Option<()> {
             chip.borrow_mut().jump(address);
 
             Some(())
         }
 
         // Call subroutine at address
-        pub fn _2(chip: Chip8InterpreterRef, address: usize) -> Option<()> {
+        pub fn _2(chip: Chip8InterpreterRef, address: u16) -> Option<()> {
             let mut chip = chip.borrow_mut();
             chip.push_stack();
             chip.jump(address);
@@ -91,7 +91,7 @@ mod opcodes {
         }
 
         // Skip next instruction if Vx == kk
-        pub fn _3(chip: Chip8InterpreterRef, args: usize) -> Option<()> {
+        pub fn _3(chip: Chip8InterpreterRef, args: u16) -> Option<()> {
             let mut chip = chip.borrow_mut();
             if chip.get_register(args >> 8) == (args & 0x0FF) as u8 {
                 chip.increase_pc();
@@ -101,7 +101,7 @@ mod opcodes {
         }
 
         // Skip next instruction if Vx != kk
-        pub fn _4(chip: Chip8InterpreterRef, args: usize) -> Option<()> {
+        pub fn _4(chip: Chip8InterpreterRef, args: u16) -> Option<()> {
             let mut chip = chip.borrow_mut();
             if chip.get_register(args >> 8) != (args & 0x0FF) as u8 {
                 chip.increase_pc();
@@ -111,7 +111,7 @@ mod opcodes {
         }
 
         // Skip next instruction if Vx == Vy
-        pub fn _5(chip: Chip8InterpreterRef, args: usize) -> Option<()> {
+        pub fn _5(chip: Chip8InterpreterRef, args: u16) -> Option<()> {
             let mut chip = chip.borrow_mut();
             if chip.get_register(args >> 16) == chip.get_register((args >> 8) & 0xF) {
                 chip.increase_pc();
@@ -121,7 +121,7 @@ mod opcodes {
         }
 
         // Set Vx = kk
-        pub fn _6(chip: Chip8InterpreterRef, args: usize) -> Option<()> {
+        pub fn _6(chip: Chip8InterpreterRef, args: u16) -> Option<()> {
             chip.borrow_mut()
                 .set_register(args >> 16, (args & 0xFF) as u8);
 
@@ -129,9 +129,8 @@ mod opcodes {
         }
 
         // Set Vx = Vx + kk
-        pub fn _7(chip: Chip8InterpreterRef, args: usize) -> Option<()> {
-            chip.borrow_mut()
-                .add_to_register(args >> 16, (args & 0xFF) as u8);
+        pub fn _7(chip: Chip8InterpreterRef, args: u16) -> Option<()> {
+            *chip.borrow_mut().get_register_mut(args >> 16) += (args >> 8) as u8;
 
             Some(())
         }
@@ -141,24 +140,77 @@ mod opcodes {
         use super::*;
 
         // Ignore, deprecated
-        pub fn _0(chip: Chip8InterpreterRef, address: usize) -> Option<()> {
+        pub fn _0(chip: Chip8InterpreterRef, address: u16) -> Option<()> {
             Some(())
         }
 
         // Clear screen
-        pub fn _e0(chip: Chip8InterpreterRef, _: usize) -> Option<()> {
+        pub fn _e0(chip: Chip8InterpreterRef, _: u16) -> Option<()> {
             Some(())
         }
 
         // Return from function
-        pub fn _ee(chip: Chip8InterpreterRef, _: usize) -> Option<()> {
+        pub fn _ee(chip: Chip8InterpreterRef, _: u16) -> Option<()> {
             chip.borrow_mut().pop_stack();
 
             Some(())
         }
     }
 
-    mod digit_8 {}
+    mod digit_8 {
+        use super::*;
+
+        // Set Vx = Vy
+        pub fn _0(chip: Chip8InterpreterRef, args: u16) -> Option<()> {
+            let mut chip = chip.borrow_mut();
+            let vy = chip.get_register((args >> 8) & 0xF);
+            chip.set_register(args >> 16, vy);
+
+            Some(())
+        }
+
+        // Sets Vx = Vx | Vy
+        pub fn _1(chip: Chip8InterpreterRef, args: u16) -> Option<()> {
+            let mut chip = chip.borrow_mut();
+            let vy = chip.get_register((args >> 8) & 0xF);
+            *chip.get_register_mut(args >> 16) |= vy;
+
+            Some(())
+        }
+
+        // Sets Vx = Vx & Vy
+        pub fn _2(chip: Chip8InterpreterRef, args: u16) -> Option<()> {
+            let mut chip = chip.borrow_mut();
+            let vy = chip.get_register((args >> 8) * 0xF);
+            *chip.get_register_mut(args >> 16) &= vy;
+
+            Some(())
+        }
+
+        pub fn _3(chip: Chip8InterpreterRef, _: u16) -> Option<()> {
+            Some(())
+        }
+
+        pub fn _4(chip: Chip8InterpreterRef, _: u16) -> Option<()> {
+            Some(())
+        }
+
+        pub fn _5(chip: Chip8InterpreterRef, _: u16) -> Option<()> {
+            Some(())
+        }
+
+        pub fn _6(chip: Chip8InterpreterRef, _: u16) -> Option<()> {
+            Some(())
+        }
+
+        pub fn _7(chip: Chip8InterpreterRef, _: u16) -> Option<()> {
+            Some(())
+        }
+
+        pub fn _E(chip: Chip8InterpreterRef, _: u16) -> Option<()> {
+            Some(())
+        }
+    }
 
     mod digit_e {}
 
