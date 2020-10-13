f0edde3d51445e60506be5ad181b268035ca853c
QualityHammer <agingllama@gmail.com>
2020-10-13 00:49:40 -0400

    Moved functions that alter the memory to the interpreter trait and implementation.

--- src/instruction_set.rs
+++ src/instruction_set.rs
@@ -1,8 +1,7 @@
 mod opcodes {
     use crate::Chip8InterpreterRef;
 
-    type Opcode = u16;
-    type Instruction = fn(Chip8InterpreterRef, u16) -> Option<()>;
+    type Instruction = fn(Chip8InterpreterRef, usize) -> Option<()>;
 
     const FIRST_DIGIT_OPCODE_COUNT: usize = 12;
     const DIGIT_0_OPCODE_COUNT: usize = 3;
@@ -12,37 +11,37 @@ mod opcodes {
 
     use first_digit::*;
 
-    const FIRST_DIGIT_OPCODES: [Opcode; 1] = [0x1];
+    const FIRST_DIGIT_OPCODES: [usize; 1] = [0x1];
     const FIRST_DIGIT_INSTRUCTIONS: [Instruction; 1] = [_1];
 
     use digit_0::*;
 
-    const DIGIT_0_OPCODES: [Opcode; DIGIT_0_OPCODE_COUNT] = [0x0, 0xE0, 0xEE];
+    const DIGIT_0_OPCODES: [usize; DIGIT_0_OPCODE_COUNT] = [0x0, 0xE0, 0xEE];
     const DIGIT_0_INSTRUCTIONS: [Instruction; DIGIT_0_OPCODE_COUNT] = [_0, _e0, _ee];
 
     use digit_8::*;
 
-    //const DIGIT_8_OPCODES: [Opcode; DIGIT_8_OPCODE_COUNT] = [];
+    //const DIGIT_8_OPCODES: [usize; DIGIT_8_OPCODE_COUNT] = [];
     //const DIGIT_8_INSTRUCTIONS: [Instruction; DIGIT_8_OPCODE_COUNT] = [];
 
     use digit_e::*;
 
-    //const DIGIT_E_OPCODES: [Opcode; DIGIT_E_OPCODE_COUNT] = [];
+    //const DIGIT_E_OPCODES: [usize; DIGIT_E_OPCODE_COUNT] = [];
     //const DIGIT_E_INSTRUCTIONS: [Instruction; DIGIT_E_OPCODE_COUNT] = [];
 
     use digit_f::*;
 
-    //const DIGIT_F_OPCODES: [Opcode; DIGIT_F_OPCODE_COUNT] = [];
+    //const DIGIT_F_OPCODES: [usize; DIGIT_F_OPCODE_COUNT] = [];
     //const DIGIT_F_INSTRUCTIONS: [Instruction; DIGIT_F_OPCODE_COUNT] = [];
 
     use std::collections::HashMap;
 
     pub struct Chip8InstructionSet {
-        first_digit: HashMap<Opcode, Instruction>,
-        digit_0: HashMap<Opcode, Instruction>,
-        digit_8: HashMap<Opcode, Instruction>,
-        digit_e: HashMap<Opcode, Instruction>,
-        digit_f: HashMap<Opcode, Instruction>,
+        first_digit: HashMap<usize, Instruction>,
+        digit_0: HashMap<usize, Instruction>,
+        digit_8: HashMap<usize, Instruction>,
+        digit_e: HashMap<usize, Instruction>,
+        digit_f: HashMap<usize, Instruction>,
     }
 
     impl Chip8InstructionSet {
@@ -51,7 +50,7 @@ mod opcodes {
             let mut digit_0 = HashMap::with_capacity(DIGIT_0_OPCODE_COUNT);
             for (op, func) in DIGIT_0_OPCODES
                 .iter()
-                .map(|o| *o as Opcode)
+                .map(|o| *o as usize)
                 .zip(DIGIT_0_INSTRUCTIONS.iter().map(|f| *f as Instruction))
             {
                 digit_0.insert(op, func);
@@ -75,46 +74,50 @@ mod opcodes {
         use super::*;
 
         // Jump to address
-        pub fn _1(chip: Chip8InterpreterRef, address: u16) -> Option<()> {
-            chip.borrow_mut().pc = address;
+        pub fn _1(chip: Chip8InterpreterRef, address: usize) -> Option<()> {
+            chip.borrow_mut().jump(address);
 
             Some(())
         }
 
         // Call subroutine at address
-        pub fn _2(chip: Chip8InterpreterRef, address: u16) -> Option<()> {
+        pub fn _2(chip: Chip8InterpreterRef, address: usize) -> Option<()> {
             let mut chip = chip.borrow_mut();
-            chip.sp += 1;
-            let sp = chip.sp as usize;
-            chip.stack[sp] = Some(chip.pc);
-            chip.pc = address;
+            chip.push_stack();
+            chip.jump(address);
 
             Some(())
         }
 
         // Skip next instruction if Vx == kk
-        pub fn _3(chip: Chip8InterpreterRef, args: u16) -> Option<()> {
+        pub fn _3(chip: Chip8InterpreterRef, args: usize) -> Option<()> {
             let mut chip = chip.borrow_mut();
-            if chip.v[(args >> 8) as usize] == (args & 0x0FF) as u8 {
-                chip.pc += 2;
+            if chip.get_register(args >> 8) == (args & 0x0FF) as u8 {
+                chip.increase_pc();
             }
 
             Some(())
         }
 
-        pub fn _4(chip: Chip8InterpreterRef, address: u16) -> Option<()> {
+        // Skip next instruction if Vx != kk
+        pub fn _4(chip: Chip8InterpreterRef, args: usize) -> Option<()> {
+            let mut chip = chip.borrow_mut();
+            if chip.get_register(args >> 8) != (args & 0x0FF) as u8 {
+                chip.increase_pc();
+            }
+
             Some(())
         }
 
-        pub fn _5(chip: Chip8InterpreterRef, address: u16) -> Option<()> {
+        pub fn _5(chip: Chip8InterpreterRef, address: usize) -> Option<()> {
             Some(())
         }
 
-        pub fn _6(chip: Chip8InterpreterRef, address: u16) -> Option<()> {
+        pub fn _6(chip: Chip8InterpreterRef, address: usize) -> Option<()> {
             Some(())
         }
 
-        pub fn _7(chip: Chip8InterpreterRef, address: u16) -> Option<()> {
+        pub fn _7(chip: Chip8InterpreterRef, address: usize) -> Option<()> {
             Some(())
         }
     }
@@ -123,23 +126,18 @@ mod opcodes {
         use super::*;
 
         // Ignore, deprecated
-        pub fn _0(chip: Chip8InterpreterRef, address: u16) -> Option<()> {
+        pub fn _0(chip: Chip8InterpreterRef, address: usize) -> Option<()> {
             Some(())
         }
 
         // Clear screen
-        pub fn _e0(chip: Chip8InterpreterRef, _: u16) -> Option<()> {
+        pub fn _e0(chip: Chip8InterpreterRef, _: usize) -> Option<()> {
             Some(())
         }
 
         // Return from function
-        pub fn _ee(chip: Chip8InterpreterRef, _: u16) -> Option<()> {
-            let mut chip = chip.borrow_mut();
-            let sp = chip.sp as usize;
-
-            chip.pc = chip.stack[sp].unwrap();
-            chip.stack[sp] = None;
-            chip.sp -= 1;
+        pub fn _ee(chip: Chip8InterpreterRef, _: usize) -> Option<()> {
+            chip.borrow_mut().pop_stack();
 
             Some(())
         }
--- src/lib.rs
+++ src/lib.rs
@@ -11,16 +11,53 @@ pub mod instruction_set;
 use std::cell::RefCell;
 use std::rc::Rc;
 
-pub type Chip8InterpreterRef = Rc<RefCell<Chip8Interpreter>>;
+pub type Chip8InterpreterRef = Rc<RefCell<dyn Chip8Interpreter>>;
 
-pub struct Chip8Interpreter {
-    pub ram: [u8; 4096],
-    pub v: [u8; 16],
-    pub i: [u8; 16],
-    pub delay_timer: u8,
-    pub sound_timer: u8,
-    pub pc: u16,
-    pub sp: u8,
-    pub stack: [Option<u16>; 16],
+pub struct Chip8Machine {
+    ram: [u8; 4096],
+    v: [u8; 16],
+    i: [u8; 16],
+    delay_timer: u8,
+    sound_timer: u8,
+    pc: usize,
+    sp: usize,
+    stack: [Option<usize>; 16],
+}
+
+impl Chip8Interpreter for Chip8Machine {
+    fn get_register(&self, address: usize) -> u8 {
+        self.v[address]
+    }
+
+    fn increase_pc(&mut self) {
+        self.pc += 1;
+    }
+
+    fn jump(&mut self, address: usize) {
+        self.pc = address;
+    }
+
+    fn pop_stack(&mut self) {
+        self.pc = self.stack[self.sp].unwrap();
+        self.stack[self.sp] = None;
+        self.sp -= 1;
+    }
+
+    fn push_stack(&mut self) {
+        self.sp += 1;
+        self.stack[self.sp] = Some(self.pc);
+    }
+}
+
+pub trait Chip8Interpreter {
+    fn get_register(&self, address: usize) -> u8;
+
+    fn increase_pc(&mut self);
+
+    fn jump(&mut self, address: usize);
+
+    fn pop_stack(&mut self);
+
+    fn push_stack(&mut self);
 }
 >>>>>>> Initial structures for the emulator and instruction set.
