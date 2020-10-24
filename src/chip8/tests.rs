b81c03ca420ae6127b892f077453d709d6502e4b
QualityHammer <agingllama@gmail.com>
2020-10-24 07:59:13 -0400

    Added a jump test.

--- src/chip8/tests.rs
+++ src/chip8/tests.rs
@@ -1,19 +1,35 @@
 use crate::chip8::Chip8Vm;
 
-#[test]
-fn test_return() {
+fn init_vm() -> Chip8Vm {
     let mut vm = Chip8Vm::new();
     vm.pc = 0x200;
-    vm.stack[0] = 0xFFFF;
+    vm.stack[0] = 0xFFF;
     vm.sp = 1;
+    vm
+}
 
-    assert_ne!(vm.pc, 0xFFFF);
+#[test]
+fn test_return() {
+    let mut vm = init_vm();
+
+    assert_ne!(vm.pc, 0xFFF);
     assert_ne!(vm.sp, 0);
     assert_ne!(vm.stack[0], 0);
 
     vm.ret(0);
 
-    assert_eq!(vm.pc, 0xFFFF);
+    assert_eq!(vm.pc, 0xFFF);
     assert_eq!(vm.sp, 0);
     assert_eq!(vm.stack[0], 0);
+}
+
+#[test]
+fn test_jump() {
+    let mut vm = init_vm();
+
+    assert_ne!(vm.pc, 0xFF0);
+
+    vm.jump(0xFF0);
+
+    assert_eq!(vm.pc, 0xFF0);
 }
