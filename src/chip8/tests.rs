6150bb05c32157524345b96a75c400ecc67f13d9
QualityHammer <agingllama@gmail.com>
2020-10-24 08:21:38 -0400

    Added labels to each test.

--- src/chip8/tests.rs
+++ src/chip8/tests.rs
@@ -1,22 +1,14 @@
 use crate::chip8::Chip8Vm;
 
-fn init_vm() -> Chip8Vm {
-    let mut vm = Chip8Vm::new();
-    vm.pc = 0x200;
-    vm.stack[0] = 0xFFF;
-    vm.sp = 1;
-    vm
-}
-
 #[test]
 fn test_jump() {
     let mut vm = init_vm();
 
-    assert_ne!(vm.pc, 0xFF0);
+    assert_ne!(vm.pc, 0xFF0, "{}", init_pc());
 
     vm.jump(0xFF0);
 
-    assert_eq!(vm.pc, 0xFF0);
+    assert_eq!(vm.pc, 0xFF0, "{}", ch_pc());
 }
 
 #[test]
@@ -25,28 +17,60 @@ fn test_call() {
 
     let prev_sp = vm.sp;
 
-    assert_ne!(vm.pc, 0xFF0);
-    assert_ne!(vm.stack[1], 0x200);
+    assert_ne!(vm.pc, 0xFF0, "{}", init_pc());
+    assert_ne!(vm.stack[1], 0x200, "{}", init_stack());
 
     vm.call(0xFF0);
 
-    assert_eq!(vm.pc, 0xFF0);
-    assert_eq!(vm.stack[1], 0x200);
-    assert_eq!(vm.stack[0], 0xFFF);
-    assert_eq!(prev_sp + 1, vm.sp);
+    assert_eq!(vm.pc, 0xFF0, "{}", ch_pc());
+    assert_eq!(vm.stack[1], 0x200, "{}", ch_stack());
+    assert_eq!(vm.stack[0], 0xFFF, "{}", ch_stack());
+    assert_eq!(prev_sp + 1, vm.sp, "{}", ch_sp());
 }
 
 #[test]
 fn test_return() {
     let mut vm = init_vm();
 
-    assert_ne!(vm.pc, 0xFFF);
-    assert_ne!(vm.sp, 0);
-    assert_ne!(vm.stack[0], 0);
+    assert_ne!(vm.pc, 0xFFF, "{}", init_pc());
+    assert_ne!(vm.sp, 0, "{}", init_sp());
+    assert_ne!(vm.stack[0], 0, "{}", init_stack());
 
     vm.ret(0);
 
-    assert_eq!(vm.pc, 0xFFF);
-    assert_eq!(vm.sp, 0);
-    assert_eq!(vm.stack[0], 0);
+    assert_eq!(vm.pc, 0xFFF, "{}", ch_pc());
+    assert_eq!(vm.sp, 0, "{}", ch_sp());
+    assert_eq!(vm.stack[0], 0, "{}", ch_stack());
+}
+
+fn init_vm() -> Chip8Vm {
+    let mut vm = Chip8Vm::new();
+    vm.pc = 0x200;
+    vm.stack[0] = 0xFFF;
+    vm.sp = 1;
+    vm
+}
+
+fn init_pc() -> &'static str {
+    "Initial program counter."
+}
+
+fn init_sp() -> &'static str {
+    "Initial stack pointer."
+}
+
+fn init_stack() -> &'static str {
+    "Initial stack."
+}
+
+fn ch_pc() -> &'static str {
+    "Testing change in program counter."
+}
+
+fn ch_sp() -> &'static str {
+    "Testing change in stack pointer."
+}
+
+fn ch_stack() -> &'static str {
+    "Testing change in stack."
 }
