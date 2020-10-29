47a16eabe1b4c6e217f2fced213b2d97da162a26
QualityHammer <agingllama@gmail.com>
2020-10-29 18:57:07 -0400

    Fixed failed merge.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 use yaci::chip8::execution;
 
 fn main() {
     execution::run_interpreter();
-=======
-fn main() {
-    println!("Hello, world!");
->>>>>>> Initial structures for the emulator and instruction set.
 }
